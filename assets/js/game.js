fd6e4fb4615837201fedd92d25a1006c46598e48
Mila Decker <milacdecker@gmail.com>
2020-10-24 17:16:11 -0400

    added first attack round and health checks

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,15 +1,46 @@
-function fight() {
-    window.alert("The fight has begun!");
-}
+var playerName = window.prompt("What is your robot's name");
+var playerHealth = 100;
+var playerAttack = 10;
 
-var playerName = window.prompt("What's your robot's name?");
+var enemyName = "Roborto";
+var enemyHealth = 50;
+var enemyAttack = 12;
 
-console.log(playerName);
+var fight = function() {
+    window.alert("Welcome to Robot Gladiators!");
 
-console.log("This logs a strong, good for leaving yourself a message");
+    // Subtract the value of 'playerAttack' from the value of 'enemyHealth' and use that result ot update the value in the 'enemyHealth' variable
+    enemyHealth = enemyHealth - playerAttack;
 
-console.log(10 + 10);
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        playerName + " attacked " + enemyName + ". " + enemyName + " now has " + enemyHealth + " health remaining."
+    );
 
-console.log("Our robot's name is " + playerName);
+    // Subtract the value of 'enemyAttack' from the value of 'playerHealth' and use that result to update the value in the 'playerHealth' variable
+    playerHealth = playerHealth - enemyAttack;
 
-// fight();
+    // Log a resulting message to the console so we know that it worked.
+    console.log(
+        enemyName + " attacked " + playerName + ". " + playerName + " now has " + playerHealth + " health remaining."
+    );
+
+    // Check enemy's health
+    if (enemyHealth <= 0) {
+        window.alert(enemyName + " has died!");
+    }
+    else {
+        window.alert(enemyName + " still has " + enemyHealth + " health left.");
+    }
+
+    // Check player's health
+    if (playerHealth <= 0) {
+        window.alert(playerName + " has died!");
+    }
+    else {
+        window.alert(playerName + " still has " + playerHealth + " health left.");
+    }
+
+};
+
+fight();
