2385ca0da13d433edc07628c5305e6179dcdc9a0
Mila Decker <milacdecker@gmail.com>
2020-10-24 17:57:13 -0400

    added playerMoney

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,6 +1,7 @@
 var playerName = window.prompt("What is your robot's name");
 var playerHealth = 100;
 var playerAttack = 10;
+var playerMoney = 10;
 
 var enemyName = "Roborto";
 var enemyHealth = 50;
