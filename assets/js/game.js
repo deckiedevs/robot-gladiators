e42ea0037d86e8db1dde5fda87f425d0532109da
Mila Decker <milacdecker@gmail.com>
2020-10-24 16:40:31 -0400

    tested console functions

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("This is an alert!  JavaScript is running!");
+function fight() {
+    window.alert("The fight has begun!");
+}
+
+var playerName = window.prompt("What's your robot's name?");
+
+console.log(playerName);
+
+console.log("This logs a strong, good for leaving yourself a message");
+
+console.log(10 + 10);
+
+console.log("Our robot's name is " + playerName);
+
+// fight();
