ec0fd43977a697421bd57ebd11f62fbf61fc6027
Mila Decker <milacdecker@gmail.com>
2020-10-26 21:40:27 -0400

    randomized fight order

--- assets/js/game.js
+++ assets/js/game.js
@@ -24,50 +24,63 @@ var fightOrSkip = function() {
 
 var fight = function(enemy) {
 
+        debugger;
+
+    var isPlayerTurn = true;
+
+    if (Math.random() > 0.5) {
+        isPlayerTurn = false;
+    }
+
     while (enemy.health > 0 && playerInfo.health > 0) {
+
+        if (isPlayerTurn) {
+            if (fightOrSkip()) {
+                break;
+            }
+
+            // remove enemy's health by subtracting the amount in the playerInfo.attack variable
+            var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
     
-        if (fightOrSkip()) {
-            break;
-        }
-
-        // remove enemy's health by subtracting the amount in the playerInfo.attack variable
-        var damage = randomNumber(playerInfo.attack - 3, playerInfo.attack);
-
-        enemy.health = Math.max(0, enemy.health - damage);
-
-        console.log(
-            playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
-        );
-
-        // check enemy's health
-        if (enemy.health <= 0) {
-            window.alert(enemy.name + " has died!");
-
-            // award player for winning
-            playerInfo.money = playerInfo.money + 20;
-            break;
-        }
-        else { 
-            window.alert(enemy.name + " still has " + enemy.health + " health left.");
-        }
-
-        // remove player's health by subtracting the amount set in the enemy.attack variable
-        var damage = randomNumber(enemy.attack - 3, enemy.attack);
-        
-        playerInfo.health = Math.max(0, playerInfo.health - damage);
-
-        console.log(
-            enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
-        );
-
-        // check player's health
-        if (playerInfo.health <= 0 ) { 
-            window.alert(playerInfo.name + " has died!");
-            break;
-        }
+            enemy.health = Math.max(0, enemy.health - damage);
+    
+            console.log(
+                playerInfo.name + " attacked " + enemy.name + ". " + enemy.name + " now has " + enemy.health + " health remaining."
+            );
+    
+            // check enemy's health
+            if (enemy.health <= 0) {
+                window.alert(enemy.name + " has died!");
+    
+                // award player for winning
+                playerInfo.money = playerInfo.money + 20;
+                break;
+            }
+            else { 
+                window.alert(enemy.name + " still has " + enemy.health + " health left.");
+            }
+        } 
         else {
-            window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            // remove player's health by subtracting the amount set in the enemy.attack variable
+            var damage = randomNumber(enemy.attack - 3, enemy.attack);
+            
+            playerInfo.health = Math.max(0, playerInfo.health - damage);
+    
+            console.log(
+                enemy.name + " attacked " + playerInfo.name + ". " + playerInfo.name + " now has " + playerInfo.health + " health remaining."
+            );
+    
+            // check player's health
+            if (playerInfo.health <= 0 ) { 
+                window.alert(playerInfo.name + " has died!");
+                break;
+            }
+            else {
+                window.alert(playerInfo.name + " still has " + playerInfo.health + " health left.");
+            }
         }
+
+        isPlayerTurn = !isPlayerTurn
     }
 };
 
@@ -120,7 +133,7 @@ var endGame = function() {
     }
 };
 
-var shop = function() {
+var shop = function() {              
     
     var shopOptionPrompt = window.prompt(
         "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store?  Please enter one: 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
@@ -161,7 +174,7 @@ var getPlayerName = function() {
     while (name === "" || name === null) {
         name = prompt("What is your robot's name?");
     }
-    console.log("Your robot's name is " + name);
+    console.log("Your robot's name is " + name + ".");
     return name;
 }
 
